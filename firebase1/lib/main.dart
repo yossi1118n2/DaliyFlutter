42fddc88185d1893bc83446ad53e09afdea69a6c
yossi1118n2 <yossimaster1401@gmail.com>
2021-11-13 11:22:56 +0900

    Firebaseをインストール中
    ・Android エミュレーターが動かない
    ・iOS cocoapod　がエラーを出す
    
    原因を調査する必要あり。

--- firebase1/lib/main.dart
+++ firebase1/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
@@ -24,7 +30,21 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FutureBuilder(
+        future: _fbApp,
+        builder: (context,snapshot){
+          if(snapshot.hasError){
+            print('You have an error! ${snapshot.error.toString()}');
+            return Text('Somethin went wrong!');
+          }else if (snapshot.hasData){
+            return MyHomePage(title: 'my home page');
+          }else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+      ),
     );
   }
 }
